1c5fb4b1853f66ee95e7e9d093a32020c8eba4c4
sathic <sathichowdhury2017@gmail.com>
2025-06-24 11:14:21 +0200

    Css Naming Change

--- components/ui/socialMediaItem.js
+++ components/ui/socialMediaItem.js
@@ -1,5 +1,5 @@
 "use client";
-import styles from "./socialMediaItem.module.css";
+import styles from "./SocialMediaItem.module.css";
 import Link from "next/link";
 import Image from "next/image";
 const SocialMediaItem = ({ url, title, icon }) => {
